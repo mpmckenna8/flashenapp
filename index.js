a5bf713b66260e668833d72ccccc13f08644317c
mpmckenna8 <mpmckenna8@me.com>
2017-05-03 20:25:27 -0700

    commit before i run standard js auto fix stuff on index.js

--- index.js
+++ index.js
@@ -34,11 +34,10 @@ imgi.onload = function () {
   canny.width = imgi.width;
   canny.height = imgi.height
 
-  //  eightBit(document.getElementById('mycanvas'), img, 114);
   ct.drawImage(imgi, 0, 0);
   //  console.log(ct.getImageData(0, 0, img.width, img.height))
   flashenSvg();
-  //  canToFlashen(ct.getImageData(0, 0, img.width, img.height));
+
 };
 
 imgi.src =  'https://i.ytimg.com/vi/1pH5c1JkhLU/hqdefault.jpg'//'http://www.dmu.ac.uk/webimages/About-DMU-images/News-images/2014/December/cyber-hack-inset.jpg'//'http://i2.kym-cdn.com/photos/images/newsfeed/000/674/934/422.jpg';
@@ -57,6 +56,14 @@ window.addEventListener('keydown',function(e){
     }
   })
 
+
+  var inputElement = document.getElementById("fileuploader");
+  inputElement.addEventListener("change", handleFiles, false);
+  function handleFiles() {
+    var fileList = this.files; /* now you can work with the file list */
+    console.log(fileList)
+      imgi.src = window.URL.createObjectURL(fileList[0]);
+  }
 
 var screenWidth;
 var screenHeight;
@@ -80,7 +87,6 @@ function flashenSvg(){
       for(x=0; x < screenWidth; x++){
       //  console.log(x)
         pixels.push(new pixel(x,y))
-
       }
     }
 
@@ -96,7 +102,6 @@ function flashenSvg(){
 var canny = document.getElementById('mycanvas')
 var ct = canny.getContext('2d')
 //console.log(ct.getImageData(0, 0, 200, 200))
-
 function drawFlash(data){
 
   svg.selectAll('rect').remove();
@@ -156,8 +161,8 @@ function setupInput(){
   var linkinput = d3.select('#linkin');
   linkinput.on('keydown', function(err,d ,e){
 
-    var linkinput = d3.select('#linkin');
-    console.log('and this val on keydown is, ', this.value)
+  //  var linkinput = d3.select('#linkin');
+    console.log('and d on keydown is, ', d)
   })
   //console.log(linkinput.value)
   linkinput.attr('value', imgi.src);
@@ -169,7 +174,7 @@ d3.select('#urlbut')
   .on('click', function(d){
     var linkinpu = d3.select('#linkin');
   //  var newlink = linkinpu.attr('value')
-    //  console.log('update image with', linkinpu[0][0].value, newlink)
+      console.log('update image with', typeof document.getElementById('linkin').value)
     imgi.src = document.getElementById('linkin').value;
 
   })
@@ -196,7 +201,7 @@ function keepsending() {
   flashenSvg();
 //  console.log('is it still checked', d3.select('#contcheck')[0][0].checked)
   setTimeout(function(elap){
-    if( !(d3.select('#contcheck')[0][0].checked) ){
+    if( !(document.getElementById("contcheck").checked) ){
       // t.stop();
       console.log('stoping the refresh')
      }
