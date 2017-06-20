e74ef2c58d9a10bc44de72028ac00b58df7b6aa1
mpmckenna8 <mpmckenna8@me.com>
2017-06-20 13:09:41 -0700

    think i fixed the merge conflicts.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+// new stuff starts here
 var flash = require('flaschenode')
 var d3 = require('d3')
 var Pixel = require('./pixer.js')
-
 var refreshDelay = 500
 
 flash.layer = 13
@@ -183,13 +183,7 @@ function setupInput () {
   linkinput.attr('value', imgi.src)
 }
 
-// Handle the action of setting a new image source.
-d3.select('#urlbut')
-  .on('click', function (d) {
-  //  var newlink = linkinpu.attr('value')
-    console.log('update image with', typeof document.getElementById('linkin').value)
-    imgi.src = document.getElementById('linkin').value
-  })
+
 
 // Handle click on the button to update the flashentaschen
 d3.select('#updateBut')
@@ -221,7 +215,7 @@ flash.show() //  flashenSvg()
   }, refreshDelay)
 }
 
-function sendToFlaschen (data) {
+function sendToFlaschen(data) {
     // console.log("Got info from the client it is: " + data);
   var datum = data // .split('\n');
   for (let d of datum) {
@@ -266,7 +260,7 @@ function drop(e) {
 
 }
 
-function handleFileDrop (filers) {
+function handleFileDropl(filers) {
   var fileList = filers /* now you can work with the file list */
   console.log(filers)
   imgi.src = window.URL.createObjectURL(fileList[0])
@@ -277,3 +271,255 @@ function handleFileDrop (filers) {
 // set up text input and will load and show inital image and allow all the
 // stuff to work
 setupInput()
+
+// new stuff ends here
+
+  // user canvas
+  var c = document.getElementById( 'mycanvas')//'c');
+  var ctx = c.getContext('2d');
+  // gif patch canvas
+
+  var gifCanvas = document.createElement('canvas');
+  var gifCtx = gifCanvas.getContext('2d');
+  var tempCanvas = document.createElement('canvas');
+  var tempCtx = tempCanvas.getContext('2d')
+
+  var url = document.getElementById('linkin');
+  // default gif
+  //url.value ='https://media2.giphy.com/media/l3vQX02uGK1Y0R4Zi/giphy.gif';
+
+  // load the default gif
+//  loadGIF();
+  var gif;
+
+  // load a gif with the current input url value
+  function loadGIF(){
+  	var oReq = new XMLHttpRequest();
+  	oReq.open("GET", url.value, true);
+  	oReq.responseType = "arraybuffer";
+
+  	oReq.onload = function (oEvent) {
+
+  	    var arrayBuffer = oReq.response; // Note: not oReq.responseText
+  	    if (arrayBuffer) {
+  	        gif = new GIF(arrayBuffer);
+  	        var frames = gif.decompressFrames(true);
+  	        console.log(gif);
+            console.log('frames = ', frames)
+  	        // render the gif
+  	        renderGIF(frames);
+  	    }
+  	};
+
+  	oReq.send(null);
+  }
+
+  var playing = false;
+  var bEdgeDetect = false;
+  var bInvert = false;
+  var bGrayscale = false;
+  var pixelPercent = 100;
+  var loadedFrames;
+  var frameIndex;
+
+  function playpause(){
+  	playing = !playing;
+  	if(playing){
+  		renderFrame();
+  	}
+  }
+
+  function renderGIF(frames){
+  	loadedFrames = frames;
+  //  console.log('frames = ', frames)
+  	frameIndex = 0;
+  //  if(frames.dims){
+    	c.width = frames[0].dims.width;
+    	c.height = frames[0].dims.height;
+
+    	gifCanvas.width = c.width;
+    	gifCanvas.height = c.height;
+
+    	if(!playing){
+    		playpause();
+    	}
+
+  //  }
+  }
+
+  var frameImageData;
+
+  function drawPatch(frame){
+    if(frame ){
+  	var dims = frame.dims;
+
+  	if(!frameImageData || dims.width != frameImageData.width || dims.height != frameImageData.height){
+  		tempCanvas.width = dims.width;
+  		tempCanvas.height = dims.height;
+  		frameImageData = tempCtx.createImageData(dims.width, dims.height);
+  	}
+
+  	// set the patch data as an override
+  	frameImageData.data.set(frame.patch);
+
+  	// draw the patch back over the canvas
+  	tempCtx.putImageData(frameImageData, 0, 0);
+
+  	gifCtx.drawImage(tempCanvas, dims.left, dims.top);
+    }
+  }
+
+  var edge = function(data, output){
+
+  	var odata = output.data;
+  	var width = gif.raw.lsd.width;
+  	var height = gif.raw.lsd.height;
+
+  	var conv = [-1, -1, -1,
+  				-1, 8, -1,
+  				-1, -1, -1];
+  	var halfside = Math.floor(3/2);
+
+  	for(var y=0; y<height; y++){
+  		for(var x=0; x<width; x++){
+
+  			var r=0, g=0, b=0;
+  			for(var cy=0; cy<3; cy++){
+  				for(var cx=0; cx<3; cx++){
+
+  					var scy = (y - halfside + cy);
+  					var scx = (x - halfside + cx);
+
+  					if(scy >= 0 && scy < height && scx >= 0 && scx < width){
+  						var src = (scy * width + scx) * 4;
+  						var f= cy * 3 + cx;
+  						r += data[src] * conv[f];
+  						g += data[src + 1] * conv[f];
+  						b += data[src + 2] * conv[f];
+  					}
+  				}
+  			}
+
+  			var i = (y * width + x) * 4;
+  			odata[i]     = r;
+  			odata[i + 1] = g;
+  			odata[i + 2] = b;
+  			odata[i + 3] = 255;
+  		}
+  	}
+
+  	return output;
+  }
+
+  var invert = function(data) {
+  	for (var i = 0; i < data.length; i += 4) {
+  		data[i]     = 255 - data[i];     // red
+  		data[i + 1] = 255 - data[i + 1]; // green
+  		data[i + 2] = 255 - data[i + 2]; // blue
+  		data[i + 3] = 255;
+  	}
+  };
+
+  var grayscale = function(data) {
+  	for (var i = 0; i < data.length; i += 4) {
+  		var avg = (data[i] + data[i +1] + data[i +2]) / 3;
+  		data[i]     = avg; // red
+  		data[i + 1] = avg; // green
+  		data[i + 2] = avg; // blue
+  		data[i + 3] = 255;
+  	}
+  };
+
+  function manipulate(){
+  	var imageData = gifCtx.getImageData(0, 0, gifCanvas.width, gifCanvas.height);
+  	var other = gifCtx.createImageData(gifCanvas.width, gifCanvas.height);
+
+  	if(bEdgeDetect){
+  		imageData = edge(imageData.data, other);
+  	}
+
+  	if(bInvert){
+  		invert(imageData.data);
+  	}
+
+  	if(bGrayscale){
+  		grayscale(imageData.data);
+  	}
+
+  	// do pixelation
+  	var pixelsX = 5 + Math.floor(pixelPercent / 100 * (c.width - 5));
+  	var pixelsY = (pixelsX * c.height) / c.width;
+
+  	if(pixelPercent != 100){
+  		ctx.mozImageSmoothingEnabled = false;
+  		ctx.webkitImageSmoothingEnabled = false;
+  		ctx.imageSmoothingEnabled = false;
+  	}
+
+  	ctx.putImageData(imageData, 0, 0);
+  	ctx.drawImage(c, 0, 0, c.width, c.height, 0, 0, pixelsX, pixelsY);
+  	ctx.drawImage(c, 0, 0, pixelsX, pixelsY, 0, 0, c.width, c.height);
+  }
+
+  function renderFrame(){
+  	// get the frame
+  	var frame = loadedFrames[frameIndex];
+
+  	var start = new Date().getTime();
+
+  	gifCtx.clearRect(0, 0, c.width, c.height);
+
+  	// draw the patch
+  	drawPatch(frame);
+
+  	// perform manipulation
+  	manipulate();
+
+  	// update the frame index
+    if( loadedFrames.length ){
+
+
+  	frameIndex++;
+  	if(frameIndex >= loadedFrames.length){
+  		frameIndex = 0;
+  	}
+
+  	var end = new Date().getTime();
+  	var diff = end - start;
+
+  	if(playing){
+  		// delay the next gif frame
+      flashenSvg();
+
+  		setTimeout(function(){
+  			renderFrame()//requestAnimationFrame(renderFrame);
+  		}, Math.max(0, Math.floor(frame.delay - diff)));
+  	}
+    }
+  }
+
+
+  d3.select('#urlbut')
+    .on('click', function(d){
+      var linkinpu = d3.select('#linkin');
+      var newlink = linkinpu.attr('text')
+      playing = false
+      var linksplit = document.getElementById('linkin').value;
+      console.log(linksplit, 'ender = ',  linksplit.split('.')[linksplit.split('.').length-1])
+
+    //  console.log('update image with', (linkinpu[0][0].value).split('.')[linksplit.length -1])
+      if( linksplit.split('.')[linksplit.split('.').length -1] !== 'gif'){
+        console.log('thinks its not a gif')
+        imgi.src = linksplit;
+      }
+      else{
+      //  imgi.src = linkinpu[0][0].value;
+        console.log('need to do gif stuff')
+    //    playing = true;
+        url.value = linksplit;
+        loadGIF()
+
+        //c = canny;
+      }
+
+    })
